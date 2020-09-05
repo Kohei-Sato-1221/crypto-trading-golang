5ff80aa0ec1f544cb479df537559c6e9b8e95d60
佐藤幸平 <Kohei-Sato-1221>
2020-09-05 12:42:34 +0900

    add okj func

---
+++ src/controller/okjTasks.go
@@ -0,0 +1,67 @@
+package controller
+
+import (
+	"config"
+	"log"
+	"okex"
+	"runtime"
+
+	"github.com/carlescere/scheduler"
+	//"runtime"
+)
+
+func StartOKJService(exchange string) {
+	log.Println("【StartOKEXService】")
+	apiClient := okex.New(config.Config.OKApiKey, config.Config.OKApiSecret, config.Config.OKPassPhrase)
+
+	placeSellOrderJob := func() {
+		log.Println("【placeSellOrderJob】start of job")
+		placeSellOrders("BTC-JPY", "BTC", apiClient)
+		placeSellOrders("ETH-JPY", "ETH", apiClient)
+		log.Println("【placeSellOrderJob】end of job")
+	}
+
+	syncSellOrderListJob := func() {
+		log.Println("【syncSellOrderListJob】Start of job")
+		shouldSkip := syncSellOrderList("BTC-JPY", apiClient)
+		if !shouldSkip {
+			goto ENDOFSYNCSELLORDER
+		}
+		shouldSkip = syncSellOrderList("ETH-JPY", apiClient)
+		if !shouldSkip {
+			goto ENDOFSYNCSELLORDER
+		}
+	ENDOFSYNCSELLORDER:
+		log.Println("【syncSellOrderListJob】End of job")
+	}
+
+	syncOrderListJob := func() {
+		log.Println("【syncOrderListJob】Start of job")
+		shouldSkip := syncOrderList("BTC-JPY", "0", exchange, apiClient)
+		if !shouldSkip {
+			goto ENDOFSELLORDER
+		}
+		shouldSkip = syncOrderList("BTC-JPY", "2", exchange, apiClient)
+		if !shouldSkip {
+			goto ENDOFSELLORDER
+		}
+		shouldSkip = syncOrderList("ETH-JPY", "0", exchange, apiClient)
+		if !shouldSkip {
+			goto ENDOFSELLORDER
+		}
+		shouldSkip = syncOrderList("ETH-JPY", "2", exchange, apiClient)
+		if !shouldSkip {
+			goto ENDOFSELLORDER
+		}
+	ENDOFSELLORDER:
+		log.Println("【syncOrderListJob】End of job")
+	}
+
+	isTest := false
+	if !isTest {
+		scheduler.Every(30).Seconds().Run(syncOrderListJob)
+		scheduler.Every(300).Seconds().Run(syncSellOrderListJob)
+		scheduler.Every(55).Seconds().Run(placeSellOrderJob)
+	}
+	runtime.Goexit()
+}
