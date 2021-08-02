bf59bd21419cb59161db0d42fff618ecb87935ad
kohei.sato <night.god.moon.garsu@gmail.com>
2021-08-02 19:56:00 +0900

    Small Fix

--- app/bitflyerTasks.go
+++ app/bitflyerTasks.go
@@ -292,7 +292,8 @@ func syncBuyOrders(product_code string, apiClient *bitflyer.APIClient) {
 		utc, _ := time.LoadLocation("UTC")
 		utc_current_date := time.Now().In(utc)
 		compareOrderDate, _ := time.ParseInLocation("2006-01-02 15:04:05", strings.Replace(order.ChildOrderDate, "T", " ", 1), time.UTC)
-		compareOrderDate = compareOrderDate.Add(2880 * time.Minute)
+		compareOrderDate = compareOrderDate.Add(60 * time.Minute)
+		//compareOrderDate = compareOrderDate.Add(2880 * time.Minute)
 		if order.Side == "BUY" && compareOrderDate.After(utc_current_date) {
 			event := models.OrderEvent{
 				OrderID:     order.ChildOrderAcceptanceID,
