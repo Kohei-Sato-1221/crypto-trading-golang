80d5a9f1ffcf6ef9e3ab64a5ada2b513477fc4cd
kohei.sato <night.god.moon.garsu@gmail.com>
2022-05-04 14:44:46 +0900

    update for 2022

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -1,43 +1,43 @@
 resource "aws_security_group" "trading_server_ec2_sg" {
-    name = "trading_server_ec2_sg"
+  name = "trading_server_ec2_sg"
 }
 
 resource "aws_security_group" "trading_db_sg" {
-    name = "trading_db_sg"
+  name = "trading_db_sg"
 }
 
 resource "aws_security_group_rule" "ingress_ssh" {
-    type = "ingress"
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    security_group_id = aws_security_group.trading_server_ec2_sg.id
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["118.11.74.11/32"]
+  security_group_id = aws_security_group.trading_server_ec2_sg.id
 }
 
 resource "aws_security_group_rule" "ingress_mysql" {
-    type = "ingress"
-    from_port = 3306
-    to_port = 3306
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] // todo limit ip address
-    security_group_id = aws_security_group.trading_server_ec2_sg.id
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"] // todo limit ip address
+  security_group_id = aws_security_group.trading_server_ec2_sg.id
 }
 
 resource "aws_security_group_rule" "sugar_egress" {
-    type = "egress"
-    from_port = "0"
-    to_port = "0"
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    security_group_id = aws_security_group.trading_server_ec2_sg.id
+  type              = "egress"
+  from_port         = "0"
+  to_port           = "0"
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.trading_server_ec2_sg.id
 }
 
 resource "aws_security_group_rule" "ingress_db" {
-    type = "ingress"
-    from_port = 3306
-    to_port = 3306
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] // todo limit ip address
-    security_group_id = aws_security_group.trading_db_sg.id
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"] // todo limit ip address
+  security_group_id = aws_security_group.trading_db_sg.id
 }
